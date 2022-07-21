8231cebdbe51f91524657b8d982660fbe61e5032
Faraz Alam <farazalam706@gmail.com>
2022-07-21 20:47:04 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,43 +5,70 @@ import Person from "./Person/person";
 import React, { Component } from "react";
 import { eventWrapper } from "@testing-library/user-event/dist/utils";
 
+// class App extends Component {
+//   state = {
+//     name: "Faraz",
+//     age: 22,
+//   };
+//   updateData = (newName) => {
+//     this.setState({
+//       name: newName,
+//        age: 19,
+//     });
+//   };
+//   changedData = (event) => {
+//     this.setState({
+//       name: event.target.value,
+//     });
+//   };
+//   render() {
+//     const style = {
+//       width: "20%",
+//       margin: "auto",
+//       border: "1px solid #eee",
+//       padding: "15px",
+//       boxShadow: "0 2px 3px #ccc",
+//       borderRadius: "5px",
+//     };
+//     return (
+//       <div className="App">
+//         <h1>Class Component</h1>
+//         <button style={style} onClick={this.updateData.bind(this, "Huraira")}>
+//           Update Data
+//         </button>
+//         <Person
+//           name={this.state.name}
+//           age={this.state.age}
+//           click={this.updateData.bind(this, "Ammad")}
+//           changed={this.changedData}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
 class App extends Component {
   state = {
     name: "Faraz",
     age: 22,
   };
-  updateData = (newName) => {
-    this.setState({
-      name: newName,
+  updateData = () => {
+    this.seState({
+      name: "Mannan",
       age: 19,
     });
   };
-  changedData = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
+
   render() {
-    const style = {
-      width: "20%",
-      margin: "auto",
-      border: "1px solid #eee",
-      padding: "15px",
-      boxShadow: "0 2px 3px #ccc",
-      borderRadius: "5px",
-    };
     return (
       <div className="App">
         <h1>Class Component</h1>
-        <button style={style} onClick={this.updateData.bind(this, "Huraira")}>
-          Update Data
-        </button>
         <Person
+          clicked={this.updateData}
           name={this.state.name}
           age={this.state.age}
-          click={this.updateData.bind(this, "Ammad")}
-          changed={this.changedData}
         />
+        <button onClick={this.updateData}>Click Me</button>
       </div>
     );
   }
--- src/Person/person.js
+++ src/Person/person.js
@@ -1,13 +1,28 @@
+// import React from "react";
+
+// const person = (props) => {
+//   return (
+//     <div className="person">
+//       <p onClick={props.click}>
+//         I am {props.name} and my age is {props.age}.
+//       </p>
+//       <input type="text" onChange={props.changed} />
+//     </div>
+//   );
+// };
+// export default person;
+
 import React from "react";
 
-const person = (props) => {
+const Person = (props) => {
   return (
     <div className="person">
-      <p onClick={props.click}>
-        I am {props.name} and my age is {props.age}.
+      <p onClick={props.clicked}>
+        Hello I am {props.name} and my age is {props.age}
       </p>
       <input type="text" onChange={props.changed} />
     </div>
   );
 };
-export default person;
+
+export default Person;
